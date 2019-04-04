3b4f9d3a34b0cf39bb6ec044ef3a8a9d8d527170
hminghe <ming.he@qq.com>
2019-04-04 13:48:10 +0800

    [!] 0.1.4

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -13,16 +13,22 @@ const Utils = {
    * 是否为 是否为 iPhone X or iPhoneXS or iPhoneXR or iPhoneXS Max
    * @returns {boolean}
    */
-  isIPhoneX() {
+  isIPhoneX () {
+    if (this.isWeb()) {
+      return (
+        typeof window !== 'undefined' &&
+        window.screen &&
+        window.screen.width &&
+        window.screen.height
+      ) && (
+        (parseInt(window.screen.width, 10) === 375 && parseInt(window.screen.height, 10) === 812) ||
+        (parseInt(window.screen.width, 10) === 414 && parseInt(window.screen.height, 10) === 896)
+      )
+    }
     const {
       deviceHeight
-    } = weex.config.env;
-    if (this.isWeb()) {
-      return typeof window !== undefined && window.screen && window.screen.width && window.screen.height &&
-        ((parseInt(window.screen.width, 10) === 375) && (parseInt(window.screen.height, 10) === 812) ||
-          (parseInt(window.screen.width, 10) === 414) && (parseInt(window.screen.height, 10) === 896));
-    }
-    return this.isIOS() && (deviceHeight === 2436 || deviceHeight === 2688 || deviceHeight == 1792);
+    } = weex.config.env
+    return this.isIOS() && (deviceHeight === 2436 || deviceHeight === 2688 || deviceHeight === 1792)
   },
   isAndroid () {
     const { platform } = weex.config.env
